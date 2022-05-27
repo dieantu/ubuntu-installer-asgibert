bc687e3f1d422dc524b571410ed7d32774098f55
dieantu <whoami@dieantu.com>
2022-05-27 19:39:38 +0200

    Faster Script

--- installer.sh
+++ installer.sh
@@ -10,21 +10,24 @@ echo ""
 echo "====================================================================="
 echo ""
 
-echo "Instal·lant calibre..."
-sudo apt-get install -y calibre
-echo "Calibre instal·lat al sistema"
-
-echo "Instal·lant aMule..."
-sudo apt-get install -y amule amule-common amule-utils-gui
-echo "aMule instal·lat al sistema"
-
-echo "Instal·lant KDE Connect"
-sudo apt-get install -y kdeconnect
-echo "KDE Connect Instal·lat al sistema"
-
-echo "Instal·lant EasyTag..."
-sudo apt-get install -y easytag
-echo "EasyTag instal·lat al sistema"
+echo "Instal·lant..."
+sudo apt-get install -y calibre \
+    amule amule-common amule-utils-gui \
+    kdeconnect \
+    easytag \
+    handbrake \
+    imagemagick \
+    mkvtoolnix mkvtoolnix-gui \
+    mediainfo-gui \
+    gimp gimp-data-extras \
+    apt-transport-https curl \
+    puddletag \
+    telegram-desktop \
+    libreoffice \
+    vlc \
+    transgui-test \
+    /
+echo "Instal·lat al sistema"
 
 echo "Instal·lant Flacon..."
 sudo add-apt-repository ppa:flacon/ppa -y
@@ -32,28 +35,7 @@ sudo apt-get update
 sudo apt-get install -y flacon
 echo "Flacon instal·lat al sistema"
 
-echo "Instal·lant HandBrake..."
-sudo apt-get install -y handbrake
-echo "HandBrake instal·lat al sistema"
-
-echo "Instal·lant Imagemagick"
-sudo apt-get install -y imagemagick
-echo "Imagemagick instal·lat al sistema"
-
-echo "Instal·lant MKVToolNix"
-sudo apt-get install -y mkvtoolnix mkvtoolnix-gui
-echo "MKVToolNix instal·lat"
-
-echo "Instal·lant MediaInfo"
-sudo apt-get install -y mediainfo-gui
-echo "MediaInfo instal·lat al sistema"
-
-echo "Instal·lant GIMP"
-sudo apt-get install -y gimp gimp-data-extras
-echo "GIMP instal·lat al sistema"
-
 echo "Instal·lant Plex Media Server"
-sudo apt install apt-transport-https curl
 curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
 echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
 sudo apt update
@@ -61,28 +43,8 @@ sudo apt-get install -y plexmediaserver
 sudo systemctl enable --now plexmediaserver
 echo "Plex Media Server instal·lat al sistema"
 
-echo "Instal·lant Puddletag"
-sudo apt-get install -y puddletag
-echo "Puddletag instal·lat al sistema"
-
-echo "Instal·lant Telegram"
-sudo apt-get install -y telegram-desktop
-echo "Telegram instal·lat al sistema"
-
-echo "Instal·lant VLC"
-sudo apt-get install -y vlc
-echo "VLC instal·lat al sistema"
-
 echo "Instal·lant avidemux"
 sudo add-apt-repository ppa:xtradeb/apps -y
 sudo apt update
 sudo apt install -y avidemux*
 echo "Instal·lant avidemux"
-
-echo "Instal·lant LibreOffice"
-sudo apt-get install y- libreoffice
-echo "LibreOffice instal·lat"
-
-echo "Instal·lant Transmission"
-sudo snap install transgui-test
-echo "Transmission instal·lat"
